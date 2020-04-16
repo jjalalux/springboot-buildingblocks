732c87f0cec7da7b68a9b57f085eefb0f688ec93
ali <mothar2@yahoo.com>
2020-04-16 10:40:36 +0100

    second commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -3,6 +3,10 @@ package com.stacksimplify.restservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Ali.Moutchou
+ *
+ */
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
