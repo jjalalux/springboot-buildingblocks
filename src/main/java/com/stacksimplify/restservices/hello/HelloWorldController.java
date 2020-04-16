58d9911d70e66790cd8f2d092c462bc57a400378
ali <mothar2@yahoo.com>
2020-04-16 11:21:30 +0100

    Hello World base

---
+++ src/main/java/com/stacksimplify/restservices/hello/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.stacksimplify.restservices.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ali.moutchou
+ *
+ */
+@RestController
+public class HelloWorldController {
+
+	//Simple Method
+	// URI - /helloworld
+	// GET 
+	// @RequestMapping(method = RequestMethod.GET, path ="/helloworld")
+	@GetMapping("/helloworld1")
+	public String helloWorld() {
+		return "Hello World from Rest Controller: HelloWorldController 1 !";
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("ali","Moutchou","Rabat");
+	}
+}
